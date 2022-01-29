d889b30b1fbc0d37484d282bf612bd8fa2130e54
Yuvraj Pal <yuvraj.pal.contractor@move.com>
2022-01-29 13:49:45 +0530

    sample route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+@Controller('/app')
 export class AppController {
-    @Get()
+    @Get('/asdf')
     getRootRoute() {
         return 'Hello there!';
+    }
+
+    @Get('/bye')
+    getByeThere() {
+        return 'bye there!'
     }
 }
